997c1a49c8d617731cb2e698c1a8c6e8ec740d5d
Ashish Gunjal <33630261+ashishgunjal@users.noreply.github.com>
2023-12-01 23:56:27 +0530

    Create PromiseAll.ts

---
+++ Promises_Async_Await/PromiseAll.ts
@@ -0,0 +1,23 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
+// Expected output: Array [3, 42, "foo"]
+
+
+
+const _promise1 = Promise.reject(3);
+const _promise2 = 42;
+const _promise3 = new Promise((resolve, reject) => {
+    setTimeout(reject, 100, 'foo');
+});
+
+Promise.all([_promise1, _promise2, _promise3]).then((values) => {
+    console.log(values);
+}).catch(error => console.log(error));
+// Expected output: Array [3, 42, "foo"]
