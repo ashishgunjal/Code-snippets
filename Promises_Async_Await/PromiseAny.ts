51e5b2a85207e9d479c4684c588a0ff464a1495a
Ashish Gunjal <33630261+ashishgunjal@users.noreply.github.com>
2023-12-02 00:14:28 +0530

    Update PromiseAny.ts

--- Promises_Async_Await/PromiseAny.ts
+++ Promises_Async_Await/PromiseAny.ts
@@ -3,7 +3,7 @@ const promiseFirst = new Promise(function (resolve, reject) {
 })
 
 const promiseSecond = new Promise(function (resolve, reject) {
-    reject('promiseSecond')
+    resolve('promiseSecond')
 })
 
 const promiseThird = new Promise(function (resolve, reject) {
@@ -14,13 +14,15 @@ const promiseFour = new Promise((resolve, reject) => {
     reject('4')
 })
 
-promiseSecond.then(resp => console.log(resp))
 
-
-promiseFour.then(resp => console.log(resp)).catch(error => console.log(error))
-
-Promise.any([promiseFirst, promiseSecond, promiseThird, promiseFour]).then(response => {
-    console.log("promise any ", response)
+Promise.any([promiseFirst, promiseSecond]).then(response => {
+    console.log("promise any 1", response)
 }).catch(error => {
-    console.log("error promise any ", error.message)
+    console.log("error promise any 1", error.message)
+})
+
+Promise.any([promiseThird, promiseFour]).then(response => {
+    console.log("promise any 2", response)
+}).catch(error => {
+    console.log("error promise any 2", error.message)
 })
